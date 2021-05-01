f969b7bdb15d26791152ee40d98ffb3b529d93a5
Sourcery AI <>
2021-05-01 05:39:32 +0000

    'Refactored by Sourcery'

--- auth.py
+++ auth.py
@@ -19,7 +19,7 @@ def signup():
 def login_post():
     email = request.form.get('email')
     password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
+    remember = bool(request.form.get('remember'))
 
     user = User.query.filter_by(email=email).first()
 
@@ -35,26 +35,26 @@ def login_post():
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
-    if request.method=='POST':
-        email = request.form.get('email')
-        name = request.form.get('name')
-        password = request.form.get('password')
+    if request.method != 'POST':
+        return render_template('signup.html')
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
 
-        user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
 
-        if user: # if a user is found, we want to redirect back to signup page so user can try again
-            flash('Email address already exists')
-            return redirect(url_for('auth.signup'))
+    if user: # if a user is found, we want to redirect back to signup page so user can try again
+        flash('Email address already exists')
+        return redirect(url_for('auth.signup'))
 
-        # create new user with the form data. Hash the password so plaintext version isn't saved.
-        new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    # create new user with the form data. Hash the password so plaintext version isn't saved.
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
 
-        # add the new user to the database
-        db.session.add(new_user)
-        db.session.commit()
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
 
-        return redirect(url_for('auth.login'))
-    return render_template('signup.html')
+    return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
 @login_required
